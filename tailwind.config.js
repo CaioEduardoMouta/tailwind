c1f3e558264d31cfb5c410c525c6ffa4aca603bb
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-10-23 15:53:04 -0300

    tail

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: [],
-  darkMode: 'media',
+  darkMode:  ['class', '[data-mode="dark"]'],
   theme: {
     extend: {
       colors: {
